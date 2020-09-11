4b61a5a2e4da984bbe64008e4dbc362e0bc66d52
yefz <1204424831@qq.com>
2020-09-11 16:24:36 +0800

    应学Git项目第四版  v4.0

--- src/test/java/com/baizhi/TestGit.java
+++ src/test/java/com/baizhi/TestGit.java
@@ -5,6 +5,7 @@ public class TestGit {
         System.out.println("dev第一操作");
         System.out.println("dev第二操作");
          System.out.println("同事的第一次操作");
+        System.out.println("我自己的第一次推送");
     }
 
 }

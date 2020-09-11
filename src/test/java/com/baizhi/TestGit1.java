f681211f02bfa8ed75f8017b7bfda86045f8eb93
yefz <1204424831@qq.com>
2020-09-11 16:41:08 +0800

    Git项目 视频模块 v2.0

--- src/test/java/com/baizhi/TestGit1.java
+++ src/test/java/com/baizhi/TestGit1.java
@@ -3,5 +3,6 @@ package com.baizhi;
 public class TestGit1 {
     public static void main(String[] args) {
         System.out.println("同事开发了用户模块");
+        System.out.println("我自己开发了视频模块");
     }
 }

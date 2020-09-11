c247f1c8b060299d9e15a2a79c1329d54238b8de
yefz <1204424831@qq.com>
2020-09-11 16:26:10 +0800

    应学Git项目第5版  v5.0

--- src/test/java/com/baizhi/TestGit.java
+++ src/test/java/com/baizhi/TestGit.java
@@ -6,6 +6,7 @@ public class TestGit {
         System.out.println("dev第二操作");
          System.out.println("同事的第一次操作");
         System.out.println("我自己的第一次推送");
+        System.out.println("我自己的第二次推送");
     }
 
 }

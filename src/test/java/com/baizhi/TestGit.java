1b77fa918f3dfb58505035c9088e9af784cbac4e
1204424831 <34115031+1204424831@users.noreply.github.com>
2020-09-11 16:20:52 +0800

    Update TestGit.java

--- src/test/java/com/baizhi/TestGit.java
+++ src/test/java/com/baizhi/TestGit.java
@@ -4,6 +4,7 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("dev第一操作");
         System.out.println("dev第二操作");
+         System.out.println("同事的第一次操作");
     }
 
 }
